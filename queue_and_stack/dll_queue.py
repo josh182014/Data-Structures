ddf83843eefadbec8a29f5b4a4f65e46f38493d4
josh182014 <josh182014@gmail.com>
2019-09-16 15:41:30 -0600

    figured out my bug, mvp complete

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,16 +7,16 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        self.storage = DoublyLinkedList(self)
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        self.storage.add_to_head(value)
+        self.storage.add_to_tail(value)
         self.size += 1
 
     def dequeue(self):
         if self.size > 0:
             self.size -= 1
-            return self.storage.remove_from_tail()
+            return self.storage.remove_from_head()
 
     def len(self):
         return self.size
