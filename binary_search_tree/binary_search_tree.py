23d13ceaef41fcc741e34d70f3c415f40665dff9
josh182014 <josh182014@gmail.com>
2019-09-18 13:57:38 -0600

    completed insertt, contains and max, figuring out what for each is supposed to do

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,22 +1,74 @@
+# from dll_queue import Queue
+# from dll_stack import Stack
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
+
+# Questions:
+# Only ints?
+# Negative numbers?
+
+# Observations
+# >= goes right
+# Need to traverse to delete
+# When deleting, the smallest child becomes parent
+
 
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):  # We're just using value, so key is value
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    # * `insert` adds the input value to the binary search tree, adhering to the
+    # rules of the ordering of elements in a binary search tree.
+    # Need to traverse to find spot to insert
+    def insert(self, value):
+        if self.value is None:
+            self.value = BinarySearchTree(value)
+        else:
+            if value >= self.value:
+                if self.right:
+                    self.right.insert(value)
+                else:
+                    self.right = BinarySearchTree(value)
+            elif value < self.value:
+                if self.left:
+                    self.left.insert(value)
+                else:
+                    self.left = BinarySearchTree(value)
+            else:
+                return "Check usage."
 
-  def contains(self, target):
-    pass
+    # * `contains` searches the binary search tree for the input value,
+    # returning a boolean indicating whether the value exists in the tree or not.
+    # Start from root and traverse the tree
+    # We can stop at the first instance of a value
+    # We know it's not found if we get to a node that doesn't have children
 
-  def get_max(self):
-    pass
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target > self.value:
+            if self.right is not None:
+                return self.right.contains(target)
+            else:
+                return False
 
-  def for_each(self, cb):
-    pass
+        elif target < self.value:
+            if self.left is not None:
+                return self.left.contains(target)
+            else:
+                return False
+
+    # * `get_max` returns the maximum value in the binary search tree.
+    def get_max(self):
+        if self.value and self.right is not None:
+            return self.right.get_max()
+        else:
+            return self.value
+
+    # * `for_each` performs a traversal of _every_ node in the tree, executing
+    # the passed-in callback function on each tree node value. There is a myriad of ways to
+    # perform tree traversal; in this case any of them should work.
+    def for_each(self, cb):
+        print('hello', cb)
